a4cdca299b8cc15acdf63543ef10ae26162ae618
Abygaelle <abygaelleb02@gmail.com>
2022-03-07 14:09:14 -0500

    finish recipeSchema

--- models/recipe.js
+++ models/recipe.js
@@ -3,7 +3,20 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const recipeSchema = new Schema ({
-  name: String,
+  name: { 
+    String,
+    required: true
+  },
+  directions: String,
+  ingredients: String,
+
+
   owner: {type: Schema.Types.ObjectId, ref: "Profile"}
 
 })
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+export {
+  Recipe
+}
